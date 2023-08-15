da4b84e58c5c621baaa4587aa333cd971c650e15
Paul Paroczai <43162542+pparocza@users.noreply.github.com>
2023-08-15 00:29:16 -0700

    steps

--- sibling-floorplan/script.js
+++ sibling-floorplan/script.js
@@ -36,7 +36,55 @@ function runPatch(){
 		fadeFilter.start(1, 50);
 		globalNow = audioCtx.currentTime;
 
+		buffer = new MyBuffer2(1, 1, audioCtx.sampleRate);
+		buffer.noise().fill(0);
+		buffer.loop = true;
+		buffer.playbackRate = 1;
 
+		lfo = new MyBuffer2(1, 1, audioCtx.sampleRate);
+		lfo.sine(1, 1).fill(0);
+		lfo.playbackRate = 2;
+		lfo.loop = true;
+	
+		gain = new MyGain(0)
+
+		inverseGain = new MyGain(-1)
+
+		pan = new MyPanner2(0);
+
+		convolver = new MyConvolver();
+		cBuffer = new MyBuffer(1, 1, audioCtx.sampleRate);
+		cBuffer.makeFm(442, 221, 0.25);
+		convolver.setBuffer(cBuffer.buffer);
+
+		convolver2 = new MyConvolver();
+		cBuffer2 = new MyBuffer(1, 1, audioCtx.sampleRate);
+		cBuffer2.makeFm(442 * 0.25, 221 * 0.251, 1);
+		convolver2.setBuffer(cBuffer2.buffer);
+
+		delay = new MyStereoDelay(0.25, 0.5, 0.25, 0.5);
+
+		rampLFO = new MyBuffer2(1, 1, audioCtx.sampleRate);
+		rampLFO.playbackRate = 0.25;
+		rampLFO.sawtooth(8).fill(0);
+		rampGain = new MyGain(0);
+		rampLFO.connect(rampGain.gain.gain);
+		rampLFO.loop = true;
+
+		buffer.connect(convolver); buffer.connect(convolver2);
+		convolver.connect(gain); convolver2.connect(gain);
+		gain.connect(pan);
+		lfo.connect(inverseGain); 
+		inverseGain.connect(pan.gainR.gain);
+		lfo.connect(gain.gain.gain); 
+		lfo.connect(pan.gainL.gain)
+		pan.connect(rampGain);
+		rampGain.connect(delay);
+		delay.connect(masterGain)
+
+		buffer.start();
+		lfo.start();
+		rampLFO.start();
 }
 
 //--------------------------------------------------------------

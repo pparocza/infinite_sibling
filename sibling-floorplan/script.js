e0e3383f13e858e4d2345b11deb8cc56d29bf435
Paul Paroczai <43162542+pparocza@users.noreply.github.com>
2023-08-23 21:29:03 -0700

    crawls

--- sibling-floorplan/script.js
+++ sibling-floorplan/script.js
@@ -52,6 +52,9 @@ function runPatch(){
 
 		pan = new MyPanner2(0);
 
+		// here is where if you did an actual frequency modulation and modulated 
+		// the modulation gain (modulate modulate modulate), you could get a more 
+		// active contour of brightness (can't since you can't fm the convolution)
 		convolver = new MyConvolver();
 		cBuffer = new MyBuffer(1, 1, audioCtx.sampleRate);
 		cBuffer.makeFm(442, 221, 0.25);
@@ -71,6 +74,9 @@ function runPatch(){
 		rampLFO.connect(rampGain.gain.gain);
 		rampLFO.loop = true;
 
+		sc = new SchwaBox("i");
+		sc.output.gain = 0.00625;
+
 		buffer.connect(convolver); buffer.connect(convolver2);
 		convolver.connect(gain); convolver2.connect(gain);
 		gain.connect(pan);
@@ -82,10 +88,46 @@ function runPatch(){
 		rampGain.connect(delay);
 		delay.connect(masterGain)
 
+		delay.connect(sc);
+		// sc.connect(masterGain)
+
+		reverb = new MyConvolver()
+		rBuf = new MyBuffer2(2, 1, audioCtx.sampleRate);
+		rBuf.noise().fill(0); rBuf.noise().fill(1);
+		rBuf.inverseSawtooth(3).multiply(0); rBuf.inverseSawtooth(3).multiply(1);
+		reverb.setBuffer(rBuf.buffer);
+		reverb.output.gain = 0.1;
+
+		sc.connect(reverb);
+		// reverb.connect(masterGain);
+		
+		squareSc = new SchwaBox("ae");
+		sqB = new MyBuffer2(1, 1, audioCtx.sampleRate);
+		sqB.floatingCycleSquare(0.1, 0.3).add(0); sqB.floatingCycleSquare(0.6, 0.8).add(0);
+		sqB.playbackRate = 0.25;
+		sqB.loop = true;
+		sqG = new MyGain(0);
+
+		rampGain.connect(squareSc); 
+		squareSc.connect(sqG); sqB.connect(sqG.gain.gain);
+		// sqG.connect(delay);
+		// sqG.connect(reverb);
+
+		invSaw = new MyBuffer2(1, 1, audioCtx.sampleRate);
+		invSaw.sine(440).fill(0);
+		invSaw.inverseSawtooth(4).multiply(0);
+		invSaw.loop = true;
+		invSaw.playbackRate = 10;
+		// invSaw.connect(masterGain);
+
 		buffer.start();
 		lfo.start();
 		rampLFO.start();
+		sqB.start();
+		invSaw.start();
 }
+
+// STRUCTURE - effects fade in - chord change? - fuzz blips
 
 //--------------------------------------------------------------
 

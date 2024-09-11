9da0766efc8b9c917ef71cb34171613d488a0843
paulparoczai <paulparoczai@gmail.com>
2024-09-11 13:52:09 -0700

    revise oscillator path

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
+export { IS_Oscillator } from "/modules/nodes/IS_Oscillator.js"
 export { InfiniteSibling } from "/modules/InfiniteSibling.js";
-export { IS_Oscillator } from "modules/nodes/IS_Oscillator.js"

b07803ee797dbb2146009dd5cf9d302c604957ce
paulparoczai <paulparoczai@gmail.com>
2024-09-11 12:50:59 -0700

    create modules directory

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-export class WDS
-{
-    isWds(string)
-    {
-        const AudioContext = window.AudioContext || window.webkitAudioContext;
-        const audioContext = new AudioContext();
-
-        // test 1
-        console.log("Run Function!");
-        // var oscillator = audioContext.createOscillator();
-        console.log("Oscillator created!");
-        return string;
-    }
-}
+export { InfiniteSibling } from "/modules/InfiniteSibling.js";

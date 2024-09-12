8e22f87dc8edfdd2ec6e9d8707ad4cbf07094cf9
paulparoczai <paulparoczai@gmail.com>
2024-09-12 16:41:12 -0700

    update index import/exports

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// audio nodes
-export { IS_Node } from "./modules/nodes/IS_Node.js";
-export { IS_Oscillator } from "./modules/nodes/IS_Oscillator.js";
-export { IS_BiquadFilterNode } from "./modules/nodes/IS_BiquadFilterNode.js";
-
 // enums
 export { IS_Interval } from "./modules/enums/IS_Interval.js";
 export { IS_KeyboardNote } from "./modules/enums/IS_KeyboardNote.js";
 export { IS_Mode } from "./modules/enums/IS_Mode.js";
+export { IS_Type } from "./modules/enums/IS_Type.js";
+
+// nodes
+// TODO: either give everything a "Node" suffix or remove this one
+export { IS_BiquadFilterNode } from "./modules/nodes/IS_BiquadFilterNode.js";
+export { IS_Gain } from "./modules/nodes/IS_Gain.js";
+export { IS_Node } from "./modules/nodes/IS_Node.js";
+export { IS_Oscillator } from "./modules/nodes/IS_Oscillator.js";
+export { IS_StartableNode } from "./modules/nodes/IS_StartableNode";
 
 // types
 export { IS_Array } from "./modules/types/IS_Array.js"
+export { IS_Object } from "./modules/types/IS_Object.js"
 export { IS_Scale } from "./modules/types/IS_Scale.js";
 export { IS_Sequence } from "./modules/types/IS_Sequence.js";
 

8a87b2f330edb23577d1403dfc1df5c896467f19
paulparoczai <paulparoczai@gmail.com>
2024-09-12 16:36:21 -0700

    IS_Node extends IS_Object

--- modules/nodes/IS_Node.js
+++ modules/nodes/IS_Node.js
@@ -1,7 +1,54 @@
-export class IS_Node
+import { IS_Object } from "../types/IS_Object.js";
+import { IS_Type } from "../enums/IS_Type.js";
+
+export class IS_Node extends IS_Object
 {
-    constructor(audioContext)
+    // TODO: all nodes have an output gain
+    constructor(siblingContext)
     {
-        this.audioContext = audioContext;
+        super(IS_Type.IS_Node);
+
+        this.siblingContext = siblingContext;
+
+        this.node = null;
+        this.params = {};
+    }
+
+    connect(audioNode)
+    {
+        if(Object.hasOwn(audioNode, "type") && audioNode.type === IS_Type.IS_Node)
+        {
+            this.node.connect(audioNode.node);
+        }
+        else
+        {
+            this.node.connect(audioNode);
+        }
+    }
+
+    connectToMainOutput()
+    {
+        this.node.connect(this.siblingContext.output);
+    }
+
+    connectToAudioDestination()
+    {
+        this.node.connect(this.siblingContext.destination);
+    }
+
+    setParamValue(key, value)
+    {
+        this.params[key] = value;
+    }
+
+    getParamValue(key)
+    {
+        return this.params[key];
+    }
+
+    setParam(key, value)
+    {
+        this.params[key] = value;
+        this.node[key].value = this.params[key];
     }
 }

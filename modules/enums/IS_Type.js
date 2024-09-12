91562b87eb1c7ca8b2bf85d61af5c6d1e16b5cde
paulparoczai <paulparoczai@gmail.com>
2024-09-12 16:34:50 -0700

    create IS_Type enum

---
+++ modules/enums/IS_Type.js
@@ -0,0 +1,11 @@
+export const IS_Type =
+{
+    IS_Node: "IS_Node",
+    IS_Array: "IS_Array",
+
+    /*
+        TODO: until IS_Object can handle type hierarchies, IS_Scale and IS_Sequence are of type "IS_Array"
+     */
+    IS_Scale: "IS_Scale",
+    IS_Sequence: "IS_Sequence"
+}

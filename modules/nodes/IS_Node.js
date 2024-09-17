6be0b0052c9736d465df7439ab025fc8bd9bccc8
paulparoczai <paulparoczai@gmail.com>
2024-09-16 17:53:15 -0700

    rename IS_Node gain setter

--- modules/nodes/IS_Node.js
+++ modules/nodes/IS_Node.js
@@ -38,7 +38,7 @@ export class IS_Node extends IS_Object
         this.output.connect(this.siblingContext.destination);
     }
 
-    set gain(value)
+    gain(value)
     {
         this.output.gain.value = value;
     }

0efa1e94e772c3ba86bffd3bd3e92c193aa4595d
paulparoczai <paulparoczai@gmail.com>
2024-09-17 20:27:26 -0700

    update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ export { IS_Type } from "./modules/enums/IS_Type.js";
 // nodes
 // TODO: either give everything a "Node" suffix or remove this one
 export { IS_BiquadFilterNode } from "./modules/nodes/IS_BiquadFilterNode.js";
+export { IS_BufferSourceNode } from "./modules/nodes/IS_BufferSource.js";
 export { IS_Gain } from "./modules/nodes/IS_Gain.js";
 export { IS_Node } from "./modules/nodes/IS_Node.js";
 export { IS_Oscillator } from "./modules/nodes/IS_Oscillator.js";
@@ -14,13 +15,16 @@ export { IS_StartableNode } from "./modules/nodes/IS_StartableNode.js";
 
 // types
 export { IS_Array } from "./modules/types/IS_Array.js"
+export { IS_Buffer } from "./modules/types/IS_Buffer.js";
 export { IS_Object } from "./modules/types/IS_Object.js"
+export { IS_Parameter } from "./modules/types/IS_Parameter.js";
 export { IS_Scale } from "./modules/types/IS_Scale.js";
 export { IS_Sequence } from "./modules/types/IS_Sequence.js";
 
 // utilities
+export * from "./modules/utilities/Constants";
 export { IS_Random } from "./modules/utilities/IS_Random.js";
-export { mToF } from "./modules/utilities/MidiToFrequency.js"
+export { Utilities } from "./modules/utilities/Utilities.js"
 
 // Infinite Sibling
 export { InfiniteSibling } from "./modules/InfiniteSibling.js";

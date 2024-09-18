498ef719a2f0172527f164946f06bf099555700a
paulparoczai <paulparoczai@gmail.com>
2024-09-17 20:23:38 -0700

    update IS_Type

--- modules/enums/IS_Type.js
+++ modules/enums/IS_Type.js
@@ -2,6 +2,9 @@ export const IS_Type =
 {
     IS_Node: "IS_Node",
     IS_Array: "IS_Array",
+    IS_Parameter: "IS_Parameter",
+    IS_Buffer: "IS_Buffer",
+    IS_BufferSource: "IS_BufferSource",
 
     /*
         TODO: until IS_Object can handle type hierarchies, IS_Scale and IS_Sequence are of type "IS_Array"

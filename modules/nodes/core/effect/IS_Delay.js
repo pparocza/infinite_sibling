f82c267cb841476817d4a7ff96d43ed37112f990
paulparoczai <paulparoczai@gmail.com>
2024-09-23 19:22:39 -0700

    create IS_Delay

---
+++ modules/nodes/core/effect/IS_Delay.js
@@ -0,0 +1,42 @@
+import {IS_Node} from "../IS_Node.js";
+
+const IS_DelayParamNames =
+{
+    delayTime: "delayTime",
+    maxDelayTime: "maxDelayTime"
+}
+
+export class IS_Delay extends IS_Node
+{
+    constructor(siblingContext, delayTime = 1, maxDelayTime = 1)
+    {
+        super();
+
+        this.paramNames = IS_DelayParamNames;
+
+        this.node = this.siblingContext.audioContext.createDelay(maxDelayTime);
+
+        this.setParam(this.paramNames.maxDelayTime, maxDelayTime);
+        this.setParam(this.paramNames.delayTime, delayTime);
+    }
+
+    get maxDelayTime()
+    {
+        return this.getParamValue(this.paramNames.maxDelayTime);
+    }
+
+    set maxDelayTime(value)
+    {
+        this.setParam(this.paramNames.maxDelayTime, value);
+    }
+
+    get delayTime()
+    {
+        return this.getParamValue(this.paramNames.delayTime);
+    }
+
+    set delayTime(value)
+    {
+        this.setParam(this.paramNames.delayTime, value);
+    }
+}

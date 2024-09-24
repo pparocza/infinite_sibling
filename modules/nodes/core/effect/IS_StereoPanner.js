4aed53795d8a85e2b3b2ee79155e22d944743ae9
paulparoczai <paulparoczai@gmail.com>
2024-09-23 19:22:46 -0700

    create IS_StereoPanner

---
+++ modules/nodes/core/effect/IS_StereoPanner.js
@@ -0,0 +1,30 @@
+import { IS_Node } from "../IS_Node.js";
+
+const IS_StereoPannerParamNames =
+{
+    pan: "pan"
+}
+
+export class IS_StereoPanner extends IS_Node
+{
+    constructor(siblingContext, pan = 0)
+    {
+        super(siblingContext);
+
+        this.node = this.siblingContext.audioContext.createStereoPanner();
+
+        this.paramNames = IS_StereoPannerParamNames;
+
+        this.setParam(this.paramNames.pan, pan);
+    }
+
+    get pan()
+    {
+        return this.getParamValue(this.paramNames.pan);
+    }
+
+    set pan(value)
+    {
+        this.setParam(this.paramNames.pan, value);
+    }
+}

db4fe980960b439a50e38917eac65428d9c9c74d
paulparoczai <paulparoczai@gmail.com>
2024-09-24 18:20:55 -0700

    delay feedback network

--- modules/nodes/core/effect/IS_Delay.js
+++ modules/nodes/core/effect/IS_Delay.js
@@ -2,21 +2,44 @@ import { IS_Node } from "../IS_Node.js";
 
 const IS_DelayParamNames =
 {
-    delayTime: "delayTime"
+    delayTime: "delayTime",
+    feedbackPercent: "feedbackPercent",
+    wetMix: "wetMix",
 }
 
 export class IS_Delay extends IS_Node
 {
-    constructor(siblingContext, delayTime = 1, maxDelayTime = 1)
+    constructor(siblingContext, delayTime = 1, feedbackPercent = 0.25,
+                wetMix = 0.5, maxDelayTime = 1)
     {
         super(siblingContext);
 
         this.paramNames = IS_DelayParamNames;
 
-        this.node = this.siblingContext.audioContext.createDelay(maxDelayTime);
-        this.node.connect(this.output);
+        this.setParamValue(this.paramNames.delayTime, delayTime);
+        this.setParamValue(this.paramNames.feedbackPercent, feedbackPercent);
+        this.setParamValue(this.paramNames.wetMix, wetMix);
 
-        this.setParam(this.paramNames.delayTime, delayTime);
+        this.node = new GainNode(this.siblingContext.audioContext);
+        this.delayNode = this.siblingContext.audioContext.createDelay(maxDelayTime);
+        this.dryGain = new GainNode(this.siblingContext.audioContext);
+        this.wetGain = new GainNode(this.siblingContext.audioContext);
+        this.feedbackGain = new GainNode(this.siblingContext.audioContext);
+
+        this.delayNode.delayTime.value = this.delayTime;
+
+        this.dryGain.gain.value = 1 - this.wetMix;
+        this.wetGain.gain.value = this.wetMix;
+        this.feedbackGain.gain.value = this.feedbackPercent;
+
+        this.node.connect(this.dryGain);
+        this.dryGain.connect(this.output);
+
+        this.node.connect(this.delayNode);
+        this.delayNode.connect(this.wetGain);
+        this.delayNode.connect(this.feedbackGain);
+        this.feedbackGain.connect(this.delayNode);
+        this.wetGain.connect(this.output);
     }
 
     get delayTime()
@@ -26,6 +49,30 @@ export class IS_Delay extends IS_Node
 
     set delayTime(value)
     {
-        this.setParam(this.paramNames.delayTime, value);
+        this.setParamValue(this.paramNames.delayTime, value);
+        this.delayNode.delayTime.value = this.delayTime;
+    }
+
+    get feedbackPercent()
+    {
+        return this.getParamValue(this.paramNames.feedbackPercent);
+    }
+
+    set feedbackPercent(value)
+    {
+        this.setParamValue(this.paramNames.feedbackPercent, value);
+        this.feedbackGain.gain.value = this.feedbackPercent;
+    }
+
+    get wetMix()
+    {
+        return this.getParamValue(this.paramNames.wetMix);
+    }
+
+    set wetMix(value)
+    {
+        this.setParamValue(this.paramNames.wetMix, value);
+        this.wetGain.gain.value = this.wetMix;
+        this.dryGain.gain.value = 1 - this.wetMix;
     }
 }

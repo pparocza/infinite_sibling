b4c5d808c667c55d1b66a48d1af1ab64856b3f9c
paulparoczai <paulparoczai@gmail.com>
2024-10-01 17:27:57 -0700

    IS_Schedule offset property

--- modules/types/Schedule/IS_Schedule.js
+++ modules/types/Schedule/IS_Schedule.js
@@ -6,21 +6,34 @@ export class IS_Schedule
     constructor()
     {
         this.schedule = [];
+        this.offset = 0;
     }
 
     scheduleStart(startableNode, time = 0, duration = -1)
     {
-        let scheduleItem = new IS_ScheduleItem(startableNode, IS_ScheduleAction.Start, time, duration);
+        let startTime = time + this.offset;
+
+        let scheduleItem = new IS_ScheduleItem
+        (
+            startableNode, IS_ScheduleAction.Start, startTime, duration
+        );
+
         this.schedule.push(scheduleItem);
     }
 
     scheduleStop(startableNode, time)
     {
-        let scheduleItem = new IS_ScheduleItem(startableNode, IS_ScheduleAction.Stop, time);
+        let stopTime = time + this.offset;
+
+        let scheduleItem = new IS_ScheduleItem
+        (
+            startableNode, IS_ScheduleAction.Stop, stopTime
+        );
+
         this.schedule.push(scheduleItem);
     }
 
-    start(timeOffset = 0)
+    start()
     {
         for(let scheduleIndex = 0; scheduleIndex < this.schedule.length; scheduleIndex++)
         {

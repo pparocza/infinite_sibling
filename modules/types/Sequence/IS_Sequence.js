eccf6f9e2df9b2da7018e1332e99ad54b655535a
paulparoczai <paulparoczai@gmail.com>
2024-10-04 17:29:48 -0700

    create IS_Sequence

---
+++ modules/types/Sequence/IS_Sequence.js
@@ -0,0 +1,49 @@
+import {IS_SequenceItem} from "./IS_SequenceItem.js";
+
+/**
+ * Arrange IS_Schedules in time
+ */
+export class IS_Sequence
+{
+    constructor(sequence = [])
+    {
+        this.sequence = [];
+    }
+
+    addSchedule(schedule, startTime, shouldCancel = true, overlap = 0)
+    {
+        let sequenceItem = new IS_SequenceItem(schedule, startTime);
+        this.sequence.push(sequenceItem);
+    }
+
+    schedule()
+    {
+        let sequenceLength = this.sequence.length;
+        let isLastInSequence = false;
+        let nextSequenceItem;
+
+        for (let sequenceIndex = 0; sequenceIndex < sequenceLength; sequenceIndex++)
+        {
+            isLastInSequence = sequenceIndex === sequenceLength - 1;
+
+            let sequenceItem = this.sequence[sequenceIndex];
+            let sequenceItemStartTime = sequenceItem.startTime;
+            let sequenceItemSchedule = sequenceItem.schedule;
+
+            sequenceItemSchedule.offset += sequenceItemStartTime;
+
+            if (!isLastInSequence)
+            {
+                nextSequenceItem = this.sequence[sequenceIndex + 1];
+
+                if (nextSequenceItem.stopPrevious)
+                {
+                    let nextStart = nextSequenceItem.startTime;
+                    let nextOverlap = nextSequenceItem.overlap;
+                    
+                    sequenceItemSchedule.duration = nextStart + nextOverlap;
+                }
+            }
+        }
+    }
+}

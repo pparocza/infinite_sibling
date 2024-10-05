551408825eda24f3b934edaaf3cc8d509dd1afe3
paulparoczai <paulparoczai@gmail.com>
2024-10-04 17:33:30 -0700

    add offset and duration to IS_ScheduleItem.schedule()

--- modules/types/Schedule/IS_ScheduleItem.js
+++ modules/types/Schedule/IS_ScheduleItem.js
@@ -10,34 +10,45 @@ export class IS_ScheduleItem
         this.duration = duration
     }
 
-    schedule()
+    schedule(offset = 0, duration = -1)
     {
         switch (this.scheduleAction)
         {
             case (IS_ScheduleAction.Start):
-                this.scheduleStart();
+                this.scheduleStart(offset, duration);
                 break;
             case (IS_ScheduleAction.Stop):
-                this.scheduleStop();
+                this.scheduleStop(offset);
                 break;
             default:
                 break;
         }
     }
 
-    scheduleStart()
+    scheduleStart(offset = 0, duration = -1)
     {
-        this.startableNode.start(this.time);
+        let startTime = this.time + offset;
+
+        // if the duration argument is less than this.duration, or this.duration is negative...
+        if (this.duration > duration || this.duration < 0)
+        {
+            // ...replace this.duration with duration argument
+            this.duration = duration;
+        }
+
+        this.startableNode.start(startTime);
 
         if (this.duration >= 0)
         {
-            this.startableNode.stop(this.time + this.duration);
+            this.startableNode.stop(startTime + this.duration);
         }
     }
 
-    scheduleStop()
+    scheduleStop(offset = 0)
     {
-        this.startableNode.stop(this.time);
+        let stopTime = offset + this.time;
+
+        this.startableNode.stop(stopTime);
     }
 
     start()

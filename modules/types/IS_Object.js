f8186a29ed4865739c439e3df39b5d828a4a8e4a
paulparoczai <paulparoczai@gmail.com>
2025-03-05 14:15:23 -0800

    IS_Object uuid

--- modules/types/IS_Object.js
+++ modules/types/IS_Object.js
@@ -1,13 +1,17 @@
+import {Utilities} from "../utilities/Utilities";
+
 export class IS_Object
 {
     // TODO: type hierarchy
     constructor(iSType)
     {
         this._iSType = iSType;
+        this._uuid = Utilities._private.GenerateUUID();
     }
 
     isISObject = true;
 
     // TODO: .isType properties (eg. IS_BiquadFilter.isBiquadFilter = true; b/c undefined will act as false in an if statement
     get iSType() { return this._iSType; };
+    get uuid() { return this._uuid; };
 }

b6a6a1c4e80f5517c9b7b00e3f9a4821af2a6318
paulparoczai <paulparoczai@gmail.com>
2025-03-12 19:28:59 -0700

    IS_BufferOperationRequestData includes channel

--- modules/types/buffer/operation/IS_BufferOperationRequestData.js
+++ modules/types/buffer/operation/IS_BufferOperationRequestData.js
@@ -2,24 +2,35 @@ export class IS_BufferOperationRequestData
 {
 	constructor
 	(
-		iSOperatorType = null, iSFunctionData = null,
-		bufferArray = null, bufferUuid = null
+		iSOperatorType = null, iSFunctionData = null, bufferUuid = null
 	)
 	{
 		this._operatorType = iSOperatorType;
 		this._functionData = iSFunctionData;
 
-		this._currentBufferArray = bufferArray;
-		this._operationArray = bufferArray !== null ? new Float32Array(bufferArray.length) : null;
+		this._currentBufferArray = null;
+		this._operationArray = null;
+		this._isSuspendedOperation = false;
 
 		this._bufferUuid = bufferUuid;
-	}
 
-	get currentBufferArray() { return this._currentBufferArray; };
-	set currentBufferArray(value) { this._currentBufferArray = value; }
+		this._channelNumber = 0;
+	}
 
 	get operatorType() { return this._operatorType; };
 	set operatorType(iSOperatorType) { this._operatorType = iSOperatorType; };
 	get functionData() { return this._functionData; };
 	set functionData(iSFunctionData) { this._functionData = iSFunctionData; };
+
+	get currentBufferArray() { return this._currentBufferArray; };
+	set currentBufferArray(value)
+	{
+		this._currentBufferArray = value;
+		this._operationArray = new Float32Array(this._currentBufferArray.length);
+	}
+
+	get isSuspendedOperation() { return this._isSuspendedOperation; }
+	set isSuspendedOperation(value) { this._isSuspendedOperation = value; }
+
+	get bufferUuid() { return this._bufferUuid; };
 }

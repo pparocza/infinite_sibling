3377af650fc1cecac00be02fd5b0d4de15c827fa
paulparoczai <paulparoczai@gmail.com>
2025-03-12 19:26:27 -0700

    update IS_ConvolverPresets to new IS_Buffer

--- modules/presets/IS_ConvolverPresets.js
+++ modules/presets/IS_ConvolverPresets.js
@@ -8,17 +8,14 @@ export class IS_ConvolverPresets extends IS_Object
         super(IS_Type.IS_Data.IS_Presets.IS_ConvolverPresets);
 
         this.convolver = IS_Convolver;
-        this.siblingContext = this.convolver.siblingContext;
     }
 
     stereoNoiseReverb(length = 3)
     {
-        let buffer = this.siblingContext.createBuffer(2, length, this.siblingContext.sampleRate);
+        let buffer = this._siblingContext.createBuffer(1, length);
 
-        buffer.noise().add(0);
-        buffer.noise().add(1);
-        buffer.inverseSawtooth(2).multiply(0);
-        buffer.inverseSawtooth(2).multiply(1);
+        buffer.noise().add();
+        buffer.inverseSawtooth(2).multiply();
 
         this.convolver.buffer = buffer;
     }

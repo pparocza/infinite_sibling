ba7c6d374d980d59753bea910cdfcafe5c4ffd5e
paulparoczai <paulparoczai@gmail.com>
2025-04-05 14:26:47 -0700

    reduce IS_NodeRegistryData

--- modules/nodes/registry/IS_NodeRegistry.js
+++ modules/nodes/registry/IS_NodeRegistry.js
@@ -12,11 +12,12 @@ export const IS_NodeRegistry =
 	registerNode(audioNode)
 	{
 		let nodeData = new IS_NodeRegistryData(audioNode);
-		nodeData._setHash(this._assignHash());
+		nodeData.setHash(this._assignHash());
 		this._registry.push(nodeData);
 		return nodeData;
 	},
 
 	getNodeData(nodeHash) { return this._registry[nodeHash]; },
+
 	_assignHash() { return this._registry.length; }
 }
--- modules/nodes/registry/IS_NodeRegistryData.js
+++ modules/nodes/registry/IS_NodeRegistryData.js
@@ -2,20 +2,13 @@ export class IS_NodeRegistryData
 {
 	constructor(audioNode)
 	{
-		this._audioNode = audioNode;
+		this._iSType = audioNode.iSType;
 
-		this._connections = [];
 		this._hash = null;
 	};
 
-	get audioNode() { return this._audioNode; };
-	get connections () { return this._connections };
+	get iSType() { return this._iSType; }
 	get hash() { return this._hash; };
 
-	_setHash(hash) { this._hash = hash; }
-
-	registerConnection(nodeData)
-	{
-		this._connections.push(nodeData);
-	}
+	setHash(hash) { this._hash = hash; }
 }
